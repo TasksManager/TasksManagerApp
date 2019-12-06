e8637514f8a9e78f71c8826903b4df4a21f4c7c2
Mikhail Semerikov <msemerikov@hotmail.com>
2019-12-06 14:23:09 +0500

    Initial commit

---
+++ TasksManagerApp/TasksManagerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TasksManagerApp
+//
+//  Created by Mikhail Semerikov on 06.12.2019.
+//  Copyright © 2019 Mikhail Semerikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
